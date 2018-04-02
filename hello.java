00120f1b8d5dd366e48cde6971319eb79d27abcf
Rajasekharreddy Pasam <rajasekharreddypasam@Rajasekharreddys-MacBook-Pro.local>
2018-04-02 17:12:19 -0400

    message

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hello{
 	public static void main(String[] args) {
 		System.out.printf("Hello world\n");
 		Scanner input = new Scanner(System.in);
-		System.out.printf("What is your name? \n");
+		System.out.printf("What is your adress? \n");
 		String name = input.nextLine();
 		System.out.printf("Welcome to the ohio columjus, %s\n", name);
 
